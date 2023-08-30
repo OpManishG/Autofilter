7a2bad8c8d9307c4dd90c46a6721027b545f70f8
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-08-30 09:49:52 -0700

    Update engine.py

--- plugins/Extra/engine.py
+++ plugins/Extra/engine.py
@@ -11,7 +11,7 @@ async def ask_ai(client, m, message):
         # Generate response using OpenAI API
         response = await ai(question)
         # Send response back to user
-        await m.edit(f"<b>🙋‍♂️ ʀᴇQᴜᴇꜱᴛᴇᴅ ʙʏ : {message.from_user.mention}\n👀 Qᴜᴇʀʏ : {query}</b>"\n\n<code>{response}</code>")
+        await m.edit(f"<b>🙋‍♂️ ʀᴇQᴜᴇꜱᴛᴇᴅ ʙʏ : {}\n👀 Qᴜᴇʀʏ : {query}</b>"\n\n<code>{response}</code>")
     except Exception as e:
         # Handle other errors
         error_message = f"An error occurred: {e}"
